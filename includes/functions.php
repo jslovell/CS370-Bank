b2c131a9abae212ea85ff8dbcaef303bdbd75d80
Josh <164248154+jslovell@users.noreply.github.com>
2024-04-09 15:02:06 -0500

    Transfer pre transfer

--- includes/functions.php
+++ includes/functions.php
@@ -8,6 +8,13 @@
         return $result;
     }
 
+    function getOneAccount($acc) {
+        global $conn;
+        $query = "SELECT account_owner, account_type, balance FROM account WHERE account_id='".$acc."'";
+        $result = mysqli_query($conn, $query);
+        return $result;
+    }
+
     function getClientInfo($user) {
         global $conn;
         $query = "SELECT f_name, l_name, email, phone_num, street_num, street, city FROM client WHERE username='".$user."'";
--- includes/transfer.php
+++ includes/transfer.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once 'db.php';
+require_once('functions.php');
 
 // Grab User submitted information
 $rec = $_POST['rec'];
@@ -17,34 +18,31 @@ $send = mysqli_real_escape_string($conn, $send);
 $stmt = $conn->prepare("SELECT balance FROM account WHERE account_id=?");
 $stmt->bind_param("s", $rec);
 $stmt->execute();
-$stmt->bind_result($rec);
+$stmt->bind_result($unused);
 $stmt->store_result();
 
-if ($stmt->num_rows == 1 && $stmt->fetch() && $send != "select") {
-    $query = mysqli_query($conn, "SELECT balance FROM account WHERE account_id='".$send."'");
-    $sendBal = mysqli_fetch_assoc($query)['balance'];
+if ($stmt->num_rows == 1 && $stmt->fetch() && $send != "select") {;
+    $sendBal = mysqli_fetch_assoc(getOneAccount($send))['balance'];
     if (is_numeric($amount) && $amount > 0 && $sendBal >= $amount) {
-        $query = mysqli_query($conn, "SELECT balance FROM account WHERE account_id='".$rec."'");
-        $recBal = mysqli_fetch_assoc($query)['balance'];
-        $newSendBal = $sendBal - (float)$amount;
-        $newRecBal = $recBal + (float)$amount;
+        $recBal = mysqli_fetch_assoc(getOneAccount($rec))['balance'];
+        $newSendBal = $sendBal - $amount;
+        $newRecBal = $recBal + $amount;
 
-        //$sqlA = "UPDATE account SET balance=".$newSendBal." WHERE account_id='".$send."'";
-        //mysqli_query($conn, $sqlA);
+        $sqlA = "UPDATE account SET balance=".$newSendBal." WHERE account_id='".$send."'";
+        mysqli_query($conn, $sqlA);
         $sqlB = "UPDATE account SET balance=".$newRecBal." WHERE account_id='".$rec."'";
         mysqli_query($conn, $sqlB);
 
         unset($_SESSION['transferError']);
         $_SESSION['transferSuccess'] = true;
-        echo $newRecBal;
-        //header('Location: ../client.php');
+        header('Location: ../client.php');
     } else {
-        $_SESSION['transferError'] = "ERROR 2";
+        $_SESSION['transferError'] = "ERROR: invalid amount";
         $_SESSION['transferSuccess'] = false;
         header('Location: ../client.php');
     }
 } else {
-    $_SESSION['transferError'] = "ERROR 1";
+    $_SESSION['transferError'] = "ERROR: invalid to/from";
     $_SESSION['transferSuccess'] = false;
     header('Location: ../client.php');
 }
