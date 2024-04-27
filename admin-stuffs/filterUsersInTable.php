a19265a0aa0e64d2f9ee3c9f00d4b760aadf3102
Josh <164248154+jslovell@users.noreply.github.com>
2024-04-27 12:37:53 -0500

    Update filterUsersInTable.php

--- admin-stuffs/filterUsersInTable.php
+++ admin-stuffs/filterUsersInTable.php
@@ -17,7 +17,7 @@ if (isset($_GET['sort'])) {
     // Display the sorted data in the table
     $asdw = 0;
     if ($result->num_rows > 0) {
-        echo "<tr><td>UsernameTHY <a onclick=\"sortTable('username')\">Username</a></td><td>F_NAME <a onclick=\"sortTable('f_name')\">f_name</a></td><td>L_NAME <a onclick=\"sortTable('l_name')\">l_name</a></td><td>EMAIL <a onclick=\"sortTable('email')\">email</a></td><td>PHONE_NUM <a onclick=\"sortTable('phone_num')\">phone_num</a></td><td>";
+        echo "<tr><td>Username <a onclick=\"sortTable('username')\">Username</a></td><td>F_NAME <a onclick=\"sortTable('f_name')\">f_name</a></td><td>L_NAME <a onclick=\"sortTable('l_name')\">l_name</a></td><td>EMAIL <a onclick=\"sortTable('email')\">email</a></td><td>PHONE_NUM <a onclick=\"sortTable('phone_num')\">phone_num</a></td><td>";
         while ($row = $result->fetch_assoc()) {
             if($asdw != 0) {
                 echo "<tr><td> <a href=\"adminOverrideClientInfo.php?AdminClient=" . $row['username'] . "\">". $row['username'] ."</a></td><td>" . $row['f_name'] . "</td><td>" . $row['l_name'] . "</td><td>" . $row['email'] . "</td><td>" . $row['phone_num'] . "</td><td>";
